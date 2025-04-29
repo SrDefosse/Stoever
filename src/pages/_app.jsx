627bcc3e289e505feafc6c23de996ae27dae416c
SrDefosse <darioarturo.hdz99@gmail.com>
2025-04-29 14:23:41 -0600

    boton flotante de navegacion

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css'; // Import global styles
+import FloatingBizSwitch from '../components/layout/FloatingBizSwitch'; // Import the button
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <FloatingBizSwitch />
+    </>
+  );
 }
