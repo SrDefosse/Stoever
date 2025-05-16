ce3f179eaf3097fd1fe51c5ff7318f3bde6b8f0c
SrDefosse <darioarturo.hdz99@gmail.com>
2025-05-16 11:13:49 -0600

    remocion de video en /leather/about

--- src/pages/leather/about.jsx
+++ src/pages/leather/about.jsx
@@ -4,7 +4,6 @@ import FooterLeather from "../../components/layout/FooterLeather";
 import Divider from "../../components/ui/Divider";
 import AboutHeroLeather from "../../components/leather/AboutHeroLeather";
 import TimelineDemo from "../../components/leather/TimelineDemo";
-import LeatherVideo from "../../components/leather/LeatherVideo";
 import LeatherPhilosophy from "../../components/leather/LeatherPhilosophy";
 import LeatherCTA from "../../components/leather/LeatherCTA";
 import LeatherContact from "../../components/leather/LeatherContact";
@@ -15,7 +14,6 @@ const LeatherAbout = () => {
       <NavbarLeather />
       <AboutHeroLeather />
       <TimelineDemo />
-      <LeatherVideo />
       <LeatherPhilosophy />
       <LeatherCTA />
       <Divider />
